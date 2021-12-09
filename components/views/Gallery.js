74accc610c4b218f865459ffdbe0932b02f403c1
mjwebb1993 <mjwebb1993@yahoo.com>
2021-12-09 17:49:50 -0600

    What State-SPA should be!!!!!!

--- components/views/Gallery.js
+++ components/views/Gallery.js
@@ -2,6 +2,7 @@ import html from "html-literal";
 
 export default st => html`
   <section id="gallery">
+    This is the ${st.view}!
     ${st.pictures.reduce(
       (html, curr) => `${html} <img src=${curr.url} alt=${curr.title}>`,
       ``
