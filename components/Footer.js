3792ecd20306a1e72511648b74e7979b11e6149f
Johan Bester <johan.bester@yahoo.com>
2021-12-16 20:27:34 -0600

    Add auto copyright date

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 import html from "html-literal";
 
+let copyrightYear = new Date().getFullYear();
+
 export default () => html`
   <footer>
-    &copy; 2021 <a href="https://savvycoders.com/">Savvy Coders</a>
+    &copy;${copyrightYear} <a href="https://savvycoders.com/">Savvy Coders</a>
   </footer>
 `;
